498772aed8b26027b41507209058551bd735f1ad
Rachanon (RCN) <143895811+SaiMaiNow@users.noreply.github.com>
2025-10-19 16:56:19 +0700

    feat: Integrate React Router and styled-components into the application
    - Added react-router-dom for routing capabilities.
    - Introduced styled-components for styling.
    - Updated App component to include routing and GlobalStyle.
    - Modified index.js to wrap App component with BrowserRouter.
    - Updated package.json and package-lock.json to include new dependencies.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
+import GlobalStyle from './GlobalStyle';
+
+import Home from "./component/Home"
+
 function App() {
   return (
-    <></>
+    <>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
   );
 }
 
