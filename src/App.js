4675258634e22c1f1447104bfc09098f87c81bdf
pppskk <namthip_m@cmu.ac.th>
2025-10-30 21:25:06 +0700

    Add new images and remove Navbar component from App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import { Routes, Route } from 'react-router-dom';
 import GlobalStyle from './GlobalStyle';
 
 import Home from "./component/Home"
-import Navbar from './component/Navbar';
 import Test from "./component/Test"
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Test" element={<Test />} />
